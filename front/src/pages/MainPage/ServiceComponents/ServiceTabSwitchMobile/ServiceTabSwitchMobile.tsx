ba6851e70bee28ad67d1fbf135245627110ae6cb
kd2ex <kountdown@mail.ru>
2024-09-14 14:27:30 +0300

    Tabs adjacements on mainPage

--- front/src/pages/MainPage/ServiceComponents/ServiceTabSwitchMobile/ServiceTabSwitchMobile.tsx
+++ front/src/pages/MainPage/ServiceComponents/ServiceTabSwitchMobile/ServiceTabSwitchMobile.tsx
@@ -11,39 +11,44 @@ const ServiceTabSwitchMobile = ({tabs, tab, setTab}) => {
     }
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                width: '100%',
-                overflow: 'auto',
-                alignItems: 'center',
-                px: 0,
-                height: 36,
-                gap: 1,
-                my: 2,
-            }}
-            className={'tabs'}
-        >
-            {tabs.map((item, index) => (
-                <Chip
-                    clickable={false}
-                    key={index}
-                    aria-label={item.value}
-                    onClick={handleClick}
-                    label={item.name}
-                    sx={{
-                        color: (theme) => item.value === tab ? 'black' : theme.palette.text.primary,
-                        bgcolor: (theme) => item.value === tab ? theme.palette.text.primary : theme.palette.background.light,
-                        '& span': {
-                            pointerEvents: 'none'
-                        },
-                        '&:hover': {
-                            backgroundColor: item.value === tab ? 'none' : '#6e6853',
-                        }
-                    }}
-                />
-            ))}
+        <Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    width: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    px: 0,
+                    py: 2,
+                    gap: 1,
+                    flexWrap: 'wrap',
+                }}
+                className={'tabs'}
+            >
+                {tabs.map((item, index) => (
+                    <Chip
+                        clickable={false}
+                        key={index}
+                        aria-label={item.value}
+                        onClick={handleClick}
+                        label={item.name}
+                        sx={{
+                            px: {xs: 0, md: 4},
+                            py: 2,
+                            color: (theme) => item.value === tab ? 'black' : theme.palette.text.primary,
+                            bgcolor: (theme) => item.value === tab ? theme.palette.text.primary : theme.palette.background.light,
+                            '& span': {
+                                pointerEvents: 'none'
+                            },
+                            '&:hover': {
+                                backgroundColor: item.value === tab ? 'none' : '#6e6853',
+                            }
+                        }}
+                    />
+                ))}
+            </Box>
         </Box>
+
     )
 
 
